68f8064264404d34dde4512d4686929ef9c5a7ac
Nelusa <nela.letochova.wxc@gmail.com>
2022-02-24 17:00:12 +0100

    3 files modified, 1 added

---
+++ cykly/ukol1.js
@@ -0,0 +1,8 @@
+/*Čti od uživatele čísla tak dlouho, dokud nezadá 0 a vždy vypiš NAAKUMULOVANÝ součet těchto čísel:
+
+Vstup: 2 Výstup: 2 Vstup: 4 Výstup: 6 Vstup: 11 Výstup: 17 Vstup: 0 */
+
+let cislo = 0;
+while (cislo == 0)
+  console.log ()
+  cislo = cislo + 
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import './podminky/ukol2.js';
 import './podminky/ukol3.js';
 import './cykly/ukol1.js';
 import './cykly/ukol2.js';
+import './cykly/ukol3.js';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
